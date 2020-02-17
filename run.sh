89e5edefa53dd61aa5b3ab8ebe93e6cbaacb6bcf
chen jian <jian.chen@iim.ltd>
2020-02-17 09:46:41 +0800

    add run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-CXX=clang++ CC=clang scons Werror=1 -j4 debug=0 asserts=0 neon=1 opencl=1 embed_kernels=1 os=android arch=arm64-v8a
+#export PATH=$PATH:/media/luyao/video_send_back/install_package/aarch64-linux-android-ndk-r17c/bin/
+#CXX=clang++ CC=clang scons Werror=1 -j4 debug=0 asserts=0 neon=1 opencl=1 embed_kernels=1 os=android arch=arm64-v8a examples=0
+
+export PATH=$PATH:/media/luyao/video_send_back/install_package/arm-linux-android-ndk-r17b/bin/
+
+CXX=clang++ CC=clang scons Werror=1 -j4 debug=0 asserts=0 neon=1 opencl=1 embed_kernels=1 os=android arch=armv7a examples=0
+#cppthreads=0
